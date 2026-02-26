acd5047310ddb3b1441ca0e2bc53f5ef19f236b4
g3344 <juan.pablo.ace.mesa@gmail.com>
2026-02-25 22:53:50 -0500

    Subir cambios

--- Practica_Final/Program.cs
+++ Practica_Final/Program.cs
@@ -1,6 +1,12 @@
-﻿
+﻿var Lista_Bibliotecas = new List<Bibliotecas>();
+Lista_Bibliotecas.Add(new Bibliotecas(){ id= 1, Nombre = "Biblioteca del paisa", Ciudad = "Medellin", correo_electronico= "Paisa@Gmail.com", Telefono = "300938AC", Activa = true});
+Lista_Bibliotecas.Add(new Bibliotecas(){ id= 2, Nombre = "Biblioteca del vago", Ciudad = "Bucaramanga", correo_electronico= "Vagosalcan@Gmail.com", Telefono = "3009219319", Activa = true});
+Lista_Bibliotecas.Add(new Bibliotecas(){ id= 3, Nombre = "Bibliotecarios yeah", Ciudad = "Medellin", correo_electronico= "yeahboiiiiiii@Gmail.com", Telefono = "1074983521", Activa = false});
 
-
+var Lista_Prestamos = new List<Prestamos>();
+Lista_Prestamos.Add(new Prestamos(){ Id= 1, Fecha_Inicio = DateTime.Parse("2025-12-12"), Fecha_Fin = DateTime.Parse("2025-12-26"), Activo = false, Estado = "Devuelto", Usuario = 1, Biblioteca =1, Libro = 2 });
+Lista_Prestamos.Add(new Prestamos(){ Id= 2, Fecha_Inicio = DateTime.Parse("2026-02-12"), Fecha_Fin = DateTime.Parse("2026-03-01"), Activo = true, Estado = "Activo", Usuario =2 = Biblioteca=1, Libro = 3});
+Lista_Prestamos.Add(new Prestamos(){ Id= 1, Fecha_Inicio = DateTime.Parse("2026-02-15"), Fecha_Fin = DateTime.Parse("2026-02-24"), Activo = true, Estado = "Atrasado", Usuario = 3, Biblioteca= 2 = Libro = 1});
 
 public class Bibliotecas{
     public int id {get; set;}
@@ -10,9 +16,9 @@ public class Bibliotecas{
     public string? Telefono {get; set;}
     public bool Activa {get; set;}
 
-    public List<Prestamos> Prestamo{get; set;}
-    public List<Libros> Libro{get; set;}
-    public List<Usuarios> Usuario{get; set;}
+    public List<Prestamos>? Prestamo{get; set;}
+    public List<Libros>? Libro{get; set;}
+    public List<Usuarios>? Usuario{get; set;}
 }
 
 public class Libros{
@@ -25,10 +31,10 @@ public int Biblioteca {get; set;}
 public int Autor {get; set;}
 public int Multa {get; set;}
 
-public Bibliotecas _Biblioteca {get; set;}
-public Autores _Autor {get; set;}
-public Multas _Multa {get; set;}
-public List<Prestamos> Prestamo{get; set;}
+public Bibliotecas? _Biblioteca {get; set;}
+public Autores? _Autor {get; set;}
+public Multas? _Multa {get; set;}
+public List<Prestamos>? Prestamo{get; set;}
 }
 
 public class Usuarios: Personas{
@@ -56,16 +62,16 @@ public class Prestamos{
     public int Libro {get; set;}
 
 
-    public Multas _Multa {get; set;}
-    public Usuarios _Usuario {get; set;}
-    public Bibliotecas _Biblioteca {get; set;}
-    public Libros _Libro {get; set;}
+    public Multas? _Multa {get; set;}
+    public Usuarios? _Usuario {get; set;}
+    public Bibliotecas? _Biblioteca {get; set;}
+    public Libros? _Libro {get; set;}
 }
 
 public class Autores: Personas{
 public string? Nacionalidad{get; set;}
 
-public List<Libros> Libro{get; set;}
+public List<Libros>? Libro{get; set;}
 }
 
 public class Categorias{
@@ -81,5 +87,5 @@ public class Multas{
     public DateTime Fecha {get; set;}
     public bool Activo {get; set;}
     
-    public List<Libros> Libro{get; set;}
+    public List<Libros>? Libro{get; set;}
 }
